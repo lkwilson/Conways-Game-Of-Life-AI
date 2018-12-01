46d46877ae4d2582e6f7e2cd479318d8bc73e7e8
Lucas Wilson <lkwilson96@gmail.com>
2018-11-30 19:27:48 -0700

    version 1.0

--- proj/setup.py
+++ proj/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="cgolai",
-    version='0.9',
+    version='1.0',
     description="AI for Conway's Game of Life",
     author='Lucas Wilson',
     author_email='lkwilson96@gmail.com',
