d01d09f86f0e1fddfbd2b055807885d9260b300e
Lucas Wilson <lkwilson96@gmail.com>
2018-12-09 06:37:04 -0700

    fix setup typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="cgolai",
-    version='1.9'
+    version='1.9',
     description="AI for Conway's Game of Life",
     author='Lucas Wilson',
     author_email='lkwilson96@gmail.com',
